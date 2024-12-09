b3005fb3fb77978e4c47cbb9f6cb8f7d52c7b6ca
Anders Nilsen <anilsen88@evilcheats.org>
2024-12-09 00:43:36 +0000

    Allow multiple unsigned binary payloads w/ selection logic based on system state

--- src/bypass.c
+++ src/bypass.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <sys/mman.h>
+#include <sys/sysinfo.h>
 
 void simulate_delay(int milliseconds) {
     struct timespec ts;
@@ -15,14 +16,41 @@ void simulate_delay(int milliseconds) {
     nanosleep(&ts, NULL);
 }
 
+int get_system_state() {
+    struct sysinfo info;
+    if (sysinfo(&info) != 0) {
+        perror("sysinfo");
+        return -1;
+    }
+    if (info.uptime < 60) {
+        return 0;
+    } else if (info.uptime < 300) {
+        return 1;
+    } else if (info.uptime < 600) {
+        return 2;
+    } else {
+        return 3;
+    }
+}
 
 void secure_boot_bypass() {
     printf("Starting Secure Boot bypass...\n");
     simulate_delay(500);
 
-    const char* unsigned_binary = "/path/to/unsigned/binary";
-    printf("Chaining loading unsigned binary: %s\n", unsigned_binary);
-    int fd = open(unsigned_binary, O_RDONLY);
+    const char* unsigned_binaries[] = {"/path/to/unsigned/binary1", "/path/to/unsigned/binary2", "/path/to/unsigned/binary3", "/path/to/unsigned/binary4"};
+    const char* selected_binary = NULL;
+
+    int system_state = get_system_state();
+    switch (system_state) {
+        case 0: selected_binary = unsigned_binaries[0]; break;
+        case 1: selected_binary = unsigned_binaries[1]; break;
+        case 2: selected_binary = unsigned_binaries[2]; break;
+        case 3: selected_binary = unsigned_binaries[3]; break;
+        default: fprintf(stderr, "Unknown system state. Exiting.\n"); exit(EXIT_FAILURE);
+    }
+
+    printf("Chaining loading unsigned binary: %s\n", selected_binary);
+    int fd = open(selected_binary, O_RDONLY);
     if (fd == -1) {
         perror("Error opening unsigned binary");
         exit(EXIT_FAILURE);
@@ -40,7 +68,7 @@ void secure_boot_bypass() {
         exit(EXIT_FAILURE);
     }
 
-    char* argv[] = {(char*)unsigned_binary, NULL};
+    char* argv[] = {(char*)selected_binary, NULL};
     char* envp[] = {NULL};
 
     execve(binary_data, argv, envp);
